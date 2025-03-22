c33d07e8c6913e13592a1c1c930c14844209da62
William Lu <lu.william.m@gmail.com>
2025-03-21 22:56:19 -0700

    Initial Commit

---
+++ YapNotes/YapNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  YapNotesApp.swift
+//  YapNotes
+//
+//  Created by William Lu on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct YapNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YapNotesTests/YapNotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  YapNotesTests.swift
+//  YapNotesTests
+//
+//  Created by William Lu on 3/21/25.
+//
+
+import Testing
+@testable import YapNotes
+
+struct YapNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
